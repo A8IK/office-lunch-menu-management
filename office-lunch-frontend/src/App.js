c6984f411344c46cd48ffbc585301175b198a942
ATIK <atikul2585@gmail.com>
2024-05-25 04:33:32 +0600

    feat:Login page and passing a function in index file endpoint(frontend)

--- office-lunch-frontend/src/App.js
+++ office-lunch-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes,BrowserRouter} from 'react-router-dom';
+import Login from './components/Login';
 import Admin from './components/Admin';
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
       <div className = "App">
         <BrowserRouter>
         <Routes>
+          <Route path="/login" element={<Login />}/>
           <Route path = "/admin" element ={<Admin/>}/>
         </Routes>
         </BrowserRouter>
--- office-lunch-frontend/src/components/Login.js
+++ office-lunch-frontend/src/components/Login.js
@@ -1,4 +1,58 @@
+import React, { useState } from 'react';
+import { login } from '../services/api';
+import { useNavigate } from 'react-router-dom';
+import './login.css';
+
 const Login = () => {
-    
-}
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [userType, setUserType] = useState('');
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            await login({ username, password, userType });
+            navigate('/admin');
+        } catch (error) {
+            console.error('Login failed:', error.response.data);
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <form onSubmit={handleLogin} className="login-form">
+                <h2><i>Login</i></h2>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required/>
+
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required/>
+
+                <select
+                    value={userType}
+                    onChange={(e) => setUserType(e.target.value)}
+                    required>
+                    <option value="" disabled>User Type</option>
+                    <option value="admin">Admin</option>
+                    <option value="employee">Employee</option>
+                </select>
+                
+                <div className="login-actions">
+                    <button type="submit" className="login-button">Login</button>
+                    <a href="/forget-password" className="forget-password-link">Forget Password?</a>
+                </div>
+            </form>
+        </div>
+    );
+};
+
 export default Login;
